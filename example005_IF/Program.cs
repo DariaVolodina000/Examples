a939f34a155d1d9e9faa1850b7439723bb53e786
Daria <dashavolodina@gmail.com>
2022-11-24 20:07:32 +0800

    3

---
+++ example005_IF/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write(" Введите имя пользователя: ");
+string userName = Console.ReadLine( );
+if (userName.ToLower() == "Маша")
+{
+    Console.WriteLine ("Ого, это же Маша!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(userName);
+}
