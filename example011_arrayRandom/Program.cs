7324f97c826e7c35162cde5f3b4b269f0ec19549
Daria <dashavolodina@gmail.com>
2022-11-28 22:29:14 +0800

    example 011 array random

---
+++ example011_arrayRandom/Program.cs
@@ -0,0 +1,54 @@
+﻿void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find)
+
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1; // -1 указано с целью" если вдруг мы будем искать число которого точно нет в нашем массиве, что бы дать понять, что этого элемента нет, и программа показала по умолчанию позицию -1
+    
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
+
+Console.WriteLine();
+
+int pos = IndexOf(array, 4);
+Console.WriteLine(pos);
+
